919d195583b0d0e8b5714406182390c49a70ca2e
Gerimae Oliva <gerimae.o@gmail.com>
2022-05-14 11:34:20 -0400

    making updates to models files

--- models/Review.js
+++ models/Review.js
@@ -2,12 +2,25 @@ const mongoose = require('mongoose');
 
 const reviewSchema = new mongoose.Schema(
     {
-        user: String,
-        foodTruck: String,
+        user: [{
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }],
+        foodTruck: [{
+            type: Schema.Types.ObjectId,
+            ref: 'FoodTruck'
+        }],
         rating: Number,
         review: String
     }
 );
+
+reviewSchema.pre('save', async function (next) {
+    await this.populate('user') && this.populate('foodTruck')
+    let aggregateRating = this.reviews.reduce((acc, review) => acc + parseInt(review.rating), 0)
+    this.currentRating = aggregateRating
+    return next();
+});
 
 const Reviews = mongoose.model('Reviews', reviewSchema);
 
