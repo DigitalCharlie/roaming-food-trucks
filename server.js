4f959c75da35012a397a7d18ae2cb0c687346fb3
Ray Kim <raykim2526@gmail.com>
2022-05-14 18:19:02 -0400

    merge conflicts resolved in server.js

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const path = require('path');
+const logger = require('morgan');
 
 require('dotenv').config();
 require('./config/database');
 
 const app = express();
 
+app.use(logger('dev'));
 app.use(express.json());
 
 app.use(require('./config/checkToken'));
 
 app.use('/users', require('./routes/users'));
-app.use('/foodtrucks', require('./routes/foodTrucks'));
-// app.use('/review', require('./routes/review'));
+app.use('/foodTrucks', require('./routes/foodTrucks'));
+app.use('/review', require('./routes/review'));
 
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
