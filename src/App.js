af870e58900a4b958ff2831b15e586800732eed0
Julio Colon <jcolon1092@gmail.com>
2022-05-14 14:28:23 -0400

    footer component and importing poppins  font into App.css, facebook, instagram, twitter icons place into src/assets, specific FooterStyles.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
-    <h1>Hello</h1>
+   <HomePage />
     </div>
   );
 }
