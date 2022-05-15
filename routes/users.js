79976df888adf9a0e8a5078761fc4778ae8f434b
mfdelcastillo <michaelfdelcastillo@icloud.com>
2022-05-15 15:57:01 -0700

    changed user post route

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/users')
 
-router.post('/user', userController.createUser);
+router.post('/', userController.createUser);
 router.get('/user/:userid', userController.getUser);
 router.put('/user/:userid', userController.updateUser);
 router.delete('/user/:userid', userController.Delete)
