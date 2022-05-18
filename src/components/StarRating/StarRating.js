b5090ae5f821eaf3e1002bd499b66493bc596048
Lucas <lucas2carlos@gmail.com>
2022-05-17 21:07:34 -0400

    making onClick ternary

--- src/components/StarRating/StarRating.js
+++ src/components/StarRating/StarRating.js
@@ -1,14 +1,13 @@
 import {useState, useEffect} from 'react'
+import styles from './StarRating.module.css'
 
 export default function StarRating() {
     //Resetes the useEffect to show CSS change
     const [starRfsh, setStarRfsh] = useState(false)
-    // these hooks  will change css of the spcecific star
+    // these hooks  will change css of the star
     const [clicked, setClicked] = useState(false)
     const [index, setIndex] = useState(5)
-    const [color, setColor] = useState('blue')
-
-    // onMouseEnter={() => {setClicked(!clicked); setIndex(idx); setRefresh(!refresh)}} onMouseLeave={() => { !clicked && setIndex(5); setRefresh(!refresh)}}
+    const [color, setColor] = useState('grey')
 
     useEffect(() => {
         if(clicked){
@@ -18,11 +17,16 @@ export default function StarRating() {
         }
     }, [starRfsh])
     return(
-        <div>
+        <div className={styles.StarRating}>
+            <div>
+                <h5>Rating</h5>
+                <button onClick={() => setIndex(-1) }>Clear</button>
+            </div>
             {
                 [...Array(5)].map((star, idx) => {
                     return(
-                        <button style={idx === index ? {color: `${color}`} : {color: 'grey'}} onClick={() => {setClicked(!clicked); setIndex(idx); setStarRfsh(!starRfsh)} }><span>&#9733;</span></button>
+                        //On Click function will change the color of the individual star in the array and any star before it, if the star clicked happens to be the first one, then it will check if clicked is true and if it is it will change it to false (changing the color)
+                        <button style={idx <= index ? {color: `${color}`} : {color: 'grey'}} onClick={() => {{index >= idx && !clicked ? setIndex(idx) || setClicked(!clicked) : index === 0 && clicked ? setClicked(!clicked) : setIndex(idx)} setStarRfsh(!starRfsh)} }><span>&#9733;</span></button>
                         
                     )
                 })
