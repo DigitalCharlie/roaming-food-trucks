6364520bd8f99ea7f7c4f9ddaf1bc20bac877a0c
Lucas <lucas2carlos@gmail.com>
2022-05-17 20:24:50 -0400

    making onClick work

--- src/components/StarRating/StarRating.js
+++ src/components/StarRating/StarRating.js
@@ -1,15 +1,28 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 export default function StarRating() {
-    // the clickd and index hook will change css of the spcecific star
-    const [clicked, ,setClicked] = useState(false)
+    //Resetes the useEffect to show CSS change
+    const [starRfsh, setStarRfsh] = useState(false)
+    // these hooks  will change css of the spcecific star
+    const [clicked, setClicked] = useState(false)
     const [index, setIndex] = useState(5)
+    const [color, setColor] = useState('blue')
+
+    // onMouseEnter={() => {setClicked(!clicked); setIndex(idx); setRefresh(!refresh)}} onMouseLeave={() => { !clicked && setIndex(5); setRefresh(!refresh)}}
+
+    useEffect(() => {
+        if(clicked){
+            setColor('red')
+        } else {
+            setColor('grey')
+        }
+    }, [starRfsh])
     return(
         <div>
             {
                 [...Array(5)].map((star, idx) => {
                     return(
-                        <button style={clicked && idx === index ? {color: '000'} : {color: 'ccc'}} onClick={() => {setClicked(!clicked); setIndex(idx)} }><span>&#9733;</span></button>
+                        <button style={idx === index ? {color: `${color}`} : {color: 'grey'}} onClick={() => {setClicked(!clicked); setIndex(idx); setStarRfsh(!starRfsh)} }><span>&#9733;</span></button>
                         
                     )
                 })
