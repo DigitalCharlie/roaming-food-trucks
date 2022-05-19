5a8dbd79ccc156ea5266a78ffb2a6f0acd06c1f0
Farah Yeazneen <farahyeazneen@Farahs-Air.home>
2022-05-18 22:21:40 -0400

    Appetizer component is done

--- src/pages/FTDetailsPage/FTDetailsPage.js
+++ src/pages/FTDetailsPage/FTDetailsPage.js
@@ -1,6 +1,6 @@
 import DrinksList from "../../components/DrinksList/DrinksList";
 import EntreeList from "../../components/EntreeList/EntreeList";
-
+import AppetizersList from "../../components/AppetizersList/AppetizersList";
 import SidesList from "../../components/SidesList/SidesList";
 import DessertsList from "../../components/DessertsList/DessertsList";
 export default function FTDetailsPage({ foodTrucks }) {
@@ -9,7 +9,7 @@ export default function FTDetailsPage({ foodTrucks }) {
             <h1>This is the Food Truck Details Page</h1>
             <DrinksList foodTrucks={foodTrucks} />
             <EntreeList foodTrucks={foodTrucks}/>
-
+            <AppetizersList foodTrucks={foodTrucks} />
             <SidesList foodTrucks={foodTrucks} />
             <DessertsList foodTrucks={foodTrucks} />
         </div>
