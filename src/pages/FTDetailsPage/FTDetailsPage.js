131d812cc87a39623b51e7e4570de3d6ae8d7f0a
Farah Yeazneen <farahyeazneen@Farahs-Air.home>
2022-05-18 21:27:07 -0400

    created appetizer list component and added it to food truckdetails page

--- src/pages/FTDetailsPage/FTDetailsPage.js
+++ src/pages/FTDetailsPage/FTDetailsPage.js
@@ -1,7 +1,14 @@
-export default function FTDetailsPage() {
+
+
+import AppetizerList from "../../components/AppetizerList/AppetizerList";
+
+export default function FTDetailsPage({foodTrucks}) {
     return (
         <div>
             <h1>This is the Food Truck Details Page</h1>
+
+
+            <AppetizerList foodTrucks={foodTrucks}/>
         </div>
     );
 };
