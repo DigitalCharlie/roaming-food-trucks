663807bfa5e9e7b02cc43eb0619d547f6ffb9d39
Lucas <lucas2carlos@gmail.com>
2022-05-21 17:59:22 -0400

    commenting out button

--- src/components/PriceList/PriceList.js
+++ src/components/PriceList/PriceList.js
@@ -2,16 +2,16 @@
 
 export default function priceList({resultPageState}){
 
-    onClick=(idx)=>{
+    const onClick=(idx)=>{
         resultPageState.priceRating[idx] = !resultPageState.priceRating[idx]
     } 
-    clearPriceOption=()=>{
+    const clearPriceOption=()=>{
         resultPageState.priceRating = resultPageState.priceRating.map((element)=>{return false}) 
     }
     
     return(
     <main>
-        <button className='clearButton' onClick={clearPriceOption()}>Clear</button>
+        {/* <button className='clearButton' onClick={clearPriceOption()}>Clear</button> */}
         <button className={`priceButton ${resultPageState.priceRating[0] ? 'selectedPrice': ''}`} onClick={onClick(0)}>$</button>
         <button className={`priceButton ${resultPageState.priceRating[1] ? 'selectedPrice': ''}`} onClick={onClick(1)}>$$</button>
         <button className={`priceButton ${resultPageState.priceRating[2] ? 'selectedPrice': ''}`} onClick={onClick(2)}>$$$</button>
