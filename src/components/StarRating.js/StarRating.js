ae4b93b268f17025415d286616186de0e2acc1be
Lucas <lucas2carlos@gmail.com>
2022-05-21 18:07:20 -0400

    fixing star rate hook

--- src/components/StarRating.js/StarRating.js
+++ src/components/StarRating.js/StarRating.js
@@ -12,22 +12,23 @@ export default function StarRating({starRate, setStarRate}) {
     useEffect(() => {
         if(clicked){
             setColor('red')
-            console.log(starRate)
+            setStarRate(index + 1)
         } else {
             setColor('grey')
+            setStarRate(0)
         }
     }, [starRfsh])
     return(
         <div className={styles.StarRating}>
             <div>
                 <h5>Rating</h5>
-                <button onClick={() => setIndex(-1) }>Clear</button>
+                <button onClick={() => {setIndex(-1); setStarRate(0)} }>Clear</button>
             </div>
             {
                 [...Array(5)].map((star, idx) => {
                     return(
                         //On Click function will change the color of the individual star in the array and any star before it, if the star clicked happens to be the first one, then it will check if clicked is true and if it is it will change it to false (changing the color)
-                        <button style={idx <= index ? {color: `${color}`} : {color: 'grey'}} onClick={() => {{index >= idx && !clicked ? setIndex(idx) || setClicked(!clicked) : index === 0 && clicked ? setClicked(!clicked) : setIndex(idx)} setStarRate(idx + 1); setStarRfsh(!starRfsh)} }><span>&#9733;</span></button>
+                        <button style={idx <= index ? {color: `${color}`} : {color: 'grey'}} onClick={() => {{index >= idx && !clicked ? setIndex(idx) || setClicked(!clicked) : index === 0 && clicked ? setClicked(!clicked) : setIndex(idx)} setStarRfsh(!starRfsh)} }><span>&#9733;</span></button>
                         
                     )
                 })
