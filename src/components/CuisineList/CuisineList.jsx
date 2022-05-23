f641a679fea255c20f23c4d4754a4919f770d87f
Lucas <lucas2carlos@gmail.com>
2022-05-23 00:15:08 -0400

    fixing cuisine query

--- src/components/CuisineList/CuisineList.jsx
+++ src/components/CuisineList/CuisineList.jsx
@@ -1,19 +1,38 @@
 import styles from './CuisineList.module.css'
 
-export default function CuisineList({ cuisine, setCuisine }) {
+export default function CuisineList({ cuisine, setCuisine, cuisineURL }) {
 
     const checkboxCuisine = (index) => {
         console.log(cuisine)
+        // this will be falsy statment if a checkbox isn't pressed because index doesn't have any value without the onClick
         const input = document.getElementById(cuisines[index])
-        if(input && input.checked){
-            // only one checkbox can be checked
-            for(let i = 0; i < cuisines.length; i++){
-                document.getElementById(cuisines[i]).checked = false
-            }
+        // first check if the cuisine querry is empty, if it isn't then check the box equivalent to that cuisine
+        if(cuisineURL == 'null') {
+            // if query is null then check if the input variable returns an element and if it is checked or not
+            if(input && input.checked){
+                // only one checkbox can be checked
+                for(let i = 0; i < cuisines.length; i++){
+                    document.getElementById(cuisines[i]).checked = false
+                }
+                input.checked = true
+                setCuisine(cuisines[index])
+            } else if(input && !input.checked) {
+                setCuisine('null')
+            } 
+        } else {
+            index = cuisines.findIndex(cuisine => cuisine == cuisineURL)
+            console.log(`new index is ${index}`)
             input.checked = true
-            setCuisine(cuisines[index])
-        } else if(input && !input.checked) {
-            setCuisine('null')
+            if(input && input.checked){
+                // only one checkbox can be checked
+                for(let i = 0; i < cuisines.length; i++){
+                    document.getElementById(cuisines[i]).checked = false
+                }
+                input.checked = true
+                setCuisine(cuisines[index])
+            } else if(input && !input.checked) {
+                setCuisine('null')
+            } 
         }
     }
 
