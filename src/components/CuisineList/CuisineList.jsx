cf0d2bf70f14ca07c731e17cadef58ec524a8608
Lucas <lucas2carlos@gmail.com>
2022-05-23 00:34:20 -0400

    fixing function

--- src/components/CuisineList/CuisineList.jsx
+++ src/components/CuisineList/CuisineList.jsx
@@ -1,38 +1,19 @@
 import styles from './CuisineList.module.css'
 
-export default function CuisineList({ cuisine, setCuisine, cuisineURL }) {
+export default function CuisineList({ cuisine, setCuisine }) {
 
     const checkboxCuisine = (index) => {
-        console.log(cuisine)
-        // this will be falsy statment if a checkbox isn't pressed because index doesn't have any value without the onClick
+        // input is undefined if onClick isn't used
         const input = document.getElementById(cuisines[index])
-        // first check if the cuisine querry is empty, if it isn't then check the box equivalent to that cuisine
-        if(cuisineURL == 'null') {
-            // if query is null then check if the input variable returns an element and if it is checked or not
-            if(input && input.checked){
-                // only one checkbox can be checked
-                for(let i = 0; i < cuisines.length; i++){
-                    document.getElementById(cuisines[i]).checked = false
-                }
-                input.checked = true
-                setCuisine(cuisines[index])
-            } else if(input && !input.checked) {
-                setCuisine('null')
-            } 
-        } else {
-            index = cuisines.findIndex(cuisine => cuisine == cuisineURL)
-            console.log(`new index is ${index}`)
+        if(input && input.checked){
+            // only one checkbox can be checked
+            for(let i = 0; i < cuisines.length; i++){
+                document.getElementById(cuisines[i]).checked = false
+            }
             input.checked = true
-            if(input && input.checked){
-                // only one checkbox can be checked
-                for(let i = 0; i < cuisines.length; i++){
-                    document.getElementById(cuisines[i]).checked = false
-                }
-                input.checked = true
-                setCuisine(cuisines[index])
-            } else if(input && !input.checked) {
-                setCuisine('null')
-            } 
+            setCuisine(cuisines[index])
+        } else if(input && !input.checked) {
+            setCuisine('null')
         }
     }
 
