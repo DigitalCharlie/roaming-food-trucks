39403e3b50a74148797bf77da258f7d0bf6233a5
Lucas <lucas2carlos@gmail.com>
2022-05-22 23:41:49 -0400

    fixing useEffect to include zipcode change and url change with cuisine

--- src/pages/ResultsPage/ResultsPage.js
+++ src/pages/ResultsPage/ResultsPage.js
@@ -13,29 +13,38 @@ export default function DashboardPage() {
     const [resultTruck, setResultTruck] = useState([])
     const [starRate, setStarRate] = useState(0)
     const [priceRate, setPriceRate] = useState(0)
-    const [cuisine, setCuisine] = useState('')
+    const [cuisine, setCuisine] = useState('null')
+    const zipURL = searchParams.get("zipcode")
     useEffect(() => {
         (async () => {
           try {
-            const data = await FoodtruckAPI.getResultTruck(searchParams.get("zipcode"))
+            const data = await FoodtruckAPI.getResultTruck(zipURL)
             setResultTruck(data)
+            setSearchParams({zipcode: zipURL, cuisine: cuisine})
           } catch(e) {
             console.log(e)
           }
         })()
-      }, [])
+      }, [zipURL])
 
     return (
         <div>
             <h1>This is the Results Page</h1>
             <button onClick={() => navigate('/')}>Home Page</button>
-            <div>
-              <h2>Filters</h2>
-              <CuisineList cuisine={cuisine} setCuisine={setCuisine} />
-              <StarRating starRate={starRate} setStarRate={setStarRate} />
-              {/* <PriceList resultPageState={resultTruck} /> */}
+            <div className='row'>
+              <div className='first-column'>
+                <h2>Filters</h2>
+                <CuisineList cuisine={cuisine} setCuisine={setCuisine} />
+                <StarRating starRate={starRate} setStarRate={setStarRate} />
+                {/* <PriceList resultPageState={resultTruck} /> */}
+              </div>
+              <div className='second-column'>
+                <ResultList resultTruck={resultTruck} starRate={starRate} priceRate={priceRate} cuisine={cuisine} />
+              </div>
+              <div className='third-column'>
+                <h2>Map Picture</h2>
+              </div>
             </div>
-            <ResultList resultTruck={resultTruck} starRate={starRate} priceRate={priceRate} cuisine={cuisine} />
         </div>
     );
 };
