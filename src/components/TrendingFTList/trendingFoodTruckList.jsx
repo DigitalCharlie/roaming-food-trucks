d67b618561949f99d2afb7c5a9b5037ce5e3c903
Lucas <lucas2carlos@gmail.com>
2022-05-23 01:55:01 -0400

    adding favorites button and uncommenting navigate button

--- src/components/TrendingFTList/trendingFoodTruckList.jsx
+++ src/components/TrendingFTList/trendingFoodTruckList.jsx
@@ -1,10 +1,10 @@
-// import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import styles from './trendingFoodTruckList.module.css'
 import { useState } from 'react'
 import * as userAPI from '../../utilities/users-api'
 
 export default function TrendingFoodTruck({ foodTrucks, user }) {
-    // const navigate = useNavigate()
+    const navigate = useNavigate()
     const [id, setId] = useState("")
     const addFavorites = async (truckId) => {
         try {
@@ -25,11 +25,11 @@ export default function TrendingFoodTruck({ foodTrucks, user }) {
                         return (
                             // returns only the first 6 food trucks
                             idx < 6 &&
-                            <div key={foodTruck._id} onClick={() => { addFavorites(foodTruck._id) }} /*onClick={() => { navigate(`${foodTruck._id}`) }}*/ >
+                            <div key={foodTruck._id} onClick={() => { navigate(`/foodtruck/detailpage/${foodTruck._id}`) }} >
                                 <img src={foodTruck.img} height='250' width='300' />
                                 <div className={styles.Banner}>
                                     <div className={styles.BannerTitle}>
-                                        <h5>{foodTruck.foodtruckName}</h5>
+                                        <h5>{foodTruck.foodTruckName} <span onClick={() => { addFavorites(foodTruck._id) }}>&#9829;</span></h5>
                                         <p>{foodTruck.currentRating ? foodTruck.currentRating.toFixed(1) : null}</p>
                                     </div>
                                     <p>Wait time</p>
