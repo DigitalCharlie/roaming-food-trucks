49b8e4650e5c131ea7c1c267f0eff29d9a5ba325
Lucas <lucas2carlos@gmail.com>
2022-05-23 21:14:17 -0400

    importing styles

--- src/components/CuisineList/CuisineList.jsx
+++ src/components/CuisineList/CuisineList.jsx
@@ -1,7 +1,9 @@
+import styles from "./Cuisine.module.css"
+
 export default function CuisineList({ handleCuisineChange }) {
-    const allCuisines = ["american", "asian-fusion", "chinese", "cuban", "ethiopian", "filipino", "french", "greek", "haitian", "indian", "japanese", "korean", "mediteranean", "mexican", "nigerian", "polish", "tex-mex", "vietnamese", "thai"];
+    const allCuisines = ["american", "asian-fusion", "chinese", "cuban", "ethiopian", "filipino", "french", "greek", "haitian", "indian", "japanese", "korean", "mediteranean", "mexican", "nigerian", "polish", "tex-mex", "thai", "vietnamese"];
     return (
-        <div>
+        <div className={styles.Cuisines}>
             {
                 allCuisines.map((element, idx) => {
                     return (
