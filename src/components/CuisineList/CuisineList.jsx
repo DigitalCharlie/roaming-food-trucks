c71a626ccf3aad3653f36a036a0ea7d569ba3d70
Digital Charlie <furman.jr@gmail.com>
2022-05-24 14:45:44 -0700

    results page loads with search from parameters.... but doesnt check the checkbox

--- src/components/CuisineList/CuisineList.jsx
+++ src/components/CuisineList/CuisineList.jsx
@@ -1,6 +1,8 @@
+import { useSearchParams } from "react-router-dom";
 import styles from "./Cuisine.module.css"
 
 export default function CuisineList({ handleCuisineChange }) {
+    const [searchParams, setSearchParams] = useSearchParams()
     const allCuisines = ["american", "asian-fusion", "chinese", "cuban", "ethiopian", "filipino", "french", "greek", "haitian", "indian", "japanese", "korean", "mediteranean", "mexican", "nigerian", "polish", "tex-mex", "thai", "vietnamese"];
     return (
         <div className={styles.Cuisines}>
@@ -9,10 +11,15 @@ export default function CuisineList({ handleCuisineChange }) {
                     return (
                         <div key={idx}>
                             <label className={styles.cuisineLabel}>
-                            <input onClick={() => {
-                                handleCuisineChange(element)
-                            }} name={element} type="checkbox" />
-                            {element}</label>
+                                <input 
+                                    onClick={() => {
+                                        handleCuisineChange(element)
+                                    }} 
+                                    name={element} 
+                                    type="checkbox"                                 
+                                />
+                                {element}
+                            </label>
                         </div>
                     )
                 })
