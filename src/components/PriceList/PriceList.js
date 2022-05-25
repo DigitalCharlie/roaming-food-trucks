74a32f3574c9a534ea3ed972aebb502891a352ed
Lucas <lucas2carlos@gmail.com>
2022-05-24 20:48:02 -0400

    A bunch of stuff

--- src/components/PriceList/PriceList.js
+++ src/components/PriceList/PriceList.js
@@ -1,20 +1,20 @@
 
 
-export default function priceList({resultPageState}){
+export default function priceList({resultPageState, setPriceRate}){
 
-    const onClick=(idx)=>{
-        resultPageState.priceRating[idx] = !resultPageState.priceRating[idx]
+    const Click=(num)=>{
+        setPriceRate(num)
     } 
     const clearPriceOption=()=>{
-        resultPageState.priceRating = resultPageState.priceRating.map((element)=>{return false}) 
+        setPriceRate(0)
     }
     
     return(
     <div>
-        {/* <button className='clearButton' onClick={clearPriceOption()}>Clear</button> */}
-        <button className={`priceButton ${resultPageState.priceRating[0] ? 'selectedPrice': ''}`} onClick={onClick(0)}>$</button>
-        <button className={`priceButton ${resultPageState.priceRating[1] ? 'selectedPrice': ''}`} onClick={onClick(1)}>$$</button>
-        <button className={`priceButton ${resultPageState.priceRating[2] ? 'selectedPrice': ''}`} onClick={onClick(2)}>$$$</button>
-        <button className={`priceButton ${resultPageState.priceRating[3] ? 'selectedPrice': ''}`} onClick={onClick(3)}>$$$$</button>
+        <button className='clearButton' onClick={clearPriceOption}>Clear</button>
+        <button className={`priceButton ${resultPageState.priceRating === 1 ? 'selectedPrice': ''}`} onClick={() => Click(1)}>$</button>
+        <button className={`priceButton ${resultPageState.priceRating === 2 ? 'selectedPrice': ''}`} onClick={() => Click(2)}>$$</button>
+        <button className={`priceButton ${resultPageState.priceRating === 3 ? 'selectedPrice': ''}`} onClick={() => Click(3)}>$$$</button>
+        <button className={`priceButton ${resultPageState.priceRating === 4 ? 'selectedPrice': ''}`} onClick={() => Click(4)}>$$$$</button>
     </div>
     )}
