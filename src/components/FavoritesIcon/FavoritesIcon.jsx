51324108d923a485ca0aae9f78e0b1fd0cb1e2bb
Digital Charlie <furman.jr@gmail.com>
2022-05-25 11:35:08 -0700

    not signed in, favorite button redirects you to login

--- src/components/FavoritesIcon/FavoritesIcon.jsx
+++ src/components/FavoritesIcon/FavoritesIcon.jsx
@@ -2,16 +2,20 @@
 import * as usersAPI from '../../utilities/users-api'
 import { useContext, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useNavigate } from 'react-router-dom'
 
 // COMPONENTS
 import UserContext from '../../context/UserContext'
 import styles from './FavoritesIcon.module.css'
+import { Navigate } from 'react-router-dom'
 
 
 export default function FavoritesIcon ({truck, circle}) {
 
 	const userContext = useContext(UserContext);
 	const user = userContext.user
+
+	const navigate = useNavigate()
 
 	const whichHeart = (truckId) => {
 		if (!user) return "fa-regular fa-heart"
@@ -27,6 +31,7 @@ export default function FavoritesIcon ({truck, circle}) {
 
 	const toggleFavorite = async (truckId) => {
         try {
+			if(!user) navigate('/signup')
             const favoriteTruck = { truck: truckId }
             const editedUser = await usersAPI.toggleFavorite(user._id, favoriteTruck)
 			userContext.setUser(editedUser)
