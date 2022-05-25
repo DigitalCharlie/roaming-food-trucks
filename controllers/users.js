ba3a7ba6f53258fdd6e62fbd6795c40f6beda30f
nguyen.danimal@gmail.com <endlesswaltz@MacBook-Pro-2.local>
2022-05-24 21:35:29 -0500

    pass user info in the reviews component

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
-const User = require('../models/user')
+const User = require('../models/User')
 
 module.exports = {
     createUser,
