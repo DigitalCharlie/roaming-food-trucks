eabe06d6b1022a5e3d8289fadfc5f2a0fb1b9c61
Digital Charlie <furman.jr@gmail.com>
2022-05-25 11:29:46 -0700

    sizable restructure to enable scroll to top on new page

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,8 +1,25 @@
+import { useState, useEffect } from "react"
+import * as foodTruckAPI from '../../utilities/foodTruck-api'
+
 import TrendingFoodTruck from "../../components/TrendingFTList/trendingFoodTruckList"
 import Hero from "../../components/Hero/HeroImage"
 import SearchBox from "../../components/SearchBox/SearchBox"
 
-export default function HomePage({ foodTrucks, user }) {
+export default function HomePage({ user }) {
+
+    const [foodTrucks, setFoodTrucks] = useState([])
+
+    useEffect(() => {
+        (async () => {
+          try {
+            const data = await foodTruckAPI.getAll() // THIS ACTUALLY ONLY GETS THE TRENDING ONES NOW
+            setFoodTrucks(data)
+          } catch (e) {
+            console.log(e)
+          }
+        })()
+      }, [])
+
     return (
         <div>
             <SearchBox />
